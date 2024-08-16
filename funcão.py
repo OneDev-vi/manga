1f78b29c8e811314c8ecc81f3caf755d92891bb2
Vin <Vini@email.com>
2024-08-16 17:13:16 -0300

    adicionando nova função ao programa ou site ou .exe

--- funcão.py
+++ funcão.py
@@ -2,3 +2,11 @@ numero_1 = int(input("dijite um numero: "))
 numero_2 = int(input("dijite outro numero: "))
 s = (numero_1 + numero_2)
 print ("primeiro numero " , numero_1 , "e o uotro" , numero_2 , "e a soma é" , s)
+#função
+a = 5
+b = 2
+if (a > b):
+    print ("A é maior que B")
+else:
+    if (a < b):
+        print ("A é menor que B")
